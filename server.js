2bb34c8dea6571a6d7d0b3dd9d442531da129b68
Nick Bountalis <nikosmpountalis@gmail.com>
2020-03-08 03:34:51 +0200

    added heroku database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-deep-84042',
     user : 'postgres',
     password : '',
     database : 'smart-brain'
