6ac7271a0db1ff1be34f5884b9dee4efdcc11179
Nick Bountalis <nikosmpountalis@gmail.com>
2020-03-08 03:39:33 +0200

    added host

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-deep-84042',
-    ssl: true
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
